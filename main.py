2018527703c7859f7bb802c072ff298e3bc6b5bb
Chell2358 <forestser00@gmail.com>
2023-04-25 10:40:34 +0900

    .

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# !pip install requests
-# !pip install bs4
-# !pip install requests
-
 import requests
+import numpy as np
 from bs4 import BeautifulSoup
 
 url = 'https://www.weather.go.kr/w/weather/forecast/mid-term.do'
 response = requests.get(url)
 region = []
+weathers = []
 
 if response.status_code == 200:
   html = response.text
@@ -18,8 +16,26 @@ else:
 soup = BeautifulSoup(html, 'html.parser')
 title = soup.title.text
 
-elements = soup.select('tbody > tr > td.midterm-city > span.sticky')
+elements = soup.select('table > tbody > tr > td')
 
 for index, element in enumerate(elements, 1) :
-  # region.append(element.text)
-	print("{} 지역 날씨: {}".format(index, element.text))
+  weathers.append(element.text)
+  print("{} 지역 날씨: {}".format(index, element.text))
+
+tmin = []
+el = soup.select('table > tbody > tr > td > span.tmn')
+for index, element in enumerate(el, 1) :
+  tmin.append(element.text)
+  print('{} : {}'.format(index, element.text))
+
+tmax = []
+els = soup.select('table > tbody > tr > td > span.tmx')
+for index, element in enumerate(els, 1) :
+  tmin.append(element.text)
+  print('{} : {}'.format(index, element.text))
+
+we = weathers[140 : -1]
+we.remove('그래프')
+
+tmin = np.reshape(tmin, (-1,8))
+tmax = np.reshape(tmax, (-1,8))
